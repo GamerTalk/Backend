5a97c60876c7d228c3a387d830375ec578b2a5b4
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-05-27 17:29:37 +0900

    initial commit

---
+++ api/views.py
@@ -0,0 +1,8 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+#from base.models import
+# from .serializers import 
+
+@api_view(['GET'])
+def hello(request):
+    return Response({'hello':'world'})
