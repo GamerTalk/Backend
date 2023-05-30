c10d5833470ad3602bb2ef844531d683c0795052
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-05-30 20:51:33 +0900

    feat: added test endpoint

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.hello),
+    path('test/', views.testhello),
 ]
--- api/views.py
+++ api/views.py
@@ -6,3 +6,8 @@ from rest_framework.decorators import api_view
 @api_view(['GET'])
 def hello(request):
     return Response({'hello':'world'})
+
+@api_view(['GET'])
+def testhello(request):
+    test = ["Jack Johnson", "John Jackson"]
+    return Response(test)
