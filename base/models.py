55cbed3de4b68088b3ef58ee0f2a5a800df38954
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-05-31 19:44:48 +0900

    998 fix tables (#4)
    * feat: added envexample
    
    * feat: updated migration

--- base/models.py
+++ base/models.py
@@ -7,7 +7,7 @@ class Users(models.Model):
     username = models.CharField(max_length=20)
     date_of_birth = models.DateField()
     about_me = models.TextField()
-    learning = models.TextField()
+    languages = models.JSONField(default='{}')
 
 class english(models.Model):
     user_uid = models.ForeignKey(Users, on_delete=models.CASCADE)
