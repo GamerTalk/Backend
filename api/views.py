a5b2731468d63bc64977c7cd8a7710197dc63b38
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-05-31 19:54:05 +0900

    feat: created serializers

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-#from base.models import
-# from .serializers import 
+from base.models import Users, english, spanish, french, german, japanese, chinese, korean, systems, genre, messages
+from .serializers import UsersSerializer, englishSerializer, spanishSerializer, frenchSerializer, germanSerializer, japaneseSerializer, chineseSerializer, koreanSerializer, systemsSerializer, genraSerializer, messagesSerializer
 
 @api_view(['GET'])
 def hello(request):
