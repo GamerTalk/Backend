6732053c18f867c222c99524e63221dd319cd616
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-06-01 15:02:18 +0900

    005 new user endpoint (#6)
    * feat: added envexample
    
    * feat: created serializers
    
    * feat: added new user endpoint
    
    * feat: added database schema image
    
    * feat: added all values in the new user endpoint
    
    * feat: able to add user data to users table
    
    * feat: added unique status to uid and username
    
    * feat: added direct reference to uid table
    
    * feat: added currently_playing to user and endpoint

---
+++ api/serializers.py
@@ -0,0 +1,58 @@
+from rest_framework import serializers
+from base.models import Users, english, spanish, french, german, japanese, chinese, korean, systems, genre, messages
+
+
+class UsersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = '__all__'
+
+class englishSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = english
+        fields = '__all__'
+
+class spanishSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = spanish
+        fields = '__all__'
+
+class frenchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = french
+        fields = '__all__'
+
+class germanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = german
+        fields = '__all__'
+
+class japaneseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = japanese
+        fields = '__all__'
+
+class chineseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = chinese
+        fields = '__all__'
+
+class koreanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = korean
+        fields = '__all__'
+
+class systemsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = systems
+        fields = '__all__'
+
+class genraSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = genre
+        fields = '__all__'
+
+class messagesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = messages
+        fields = '__all__'
