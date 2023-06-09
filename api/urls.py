92fc111aed1b5e98df2ae20a0ddc10b85ce7f11e
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-06-09 15:15:28 +0900

    024 edit user endpoint (#25)
    * WIP: edit removes the user first
    
    * feat: added edit endpoint
    
    * feat: added edit user endpoint

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.hello),
     path('test/', views.testhello),
     path('new-user/', views.NewUser),
+    path('edit-user/', views.EditUser),
     # path('all-users/', views.allUsers),
     path('filter-users/', views.filterUsers),
     path('user-info/', views.userInfo),
--- api/views.py
+++ api/views.py
@@ -271,3 +271,140 @@ def NewUser(request):
         }
     )
 
+@api_view(["PATCH"])
+def EditUser(request):
+    # sample body
+    # {
+    #     "uid": "TESTUSER5",
+    #     "username": "TESTUSER5",
+    #     "about_me": "I was born in a log cabin.",
+    #     "fluent": ["english", "spanish"],
+    #     "learning": [{"language":"german", "level": 2}, {"language":"japanese", "level": 3}],
+    #     "date_of_birth": "1999-01-01",
+    #     "systems": ["playstation","PC"],
+    #     "genre": ["shooters", "survival", "fighting"],
+    #     "currently_playing": "I am currently playing COD MW2, Fortnite, and some Ark Survival",
+    #     "region": "north america"
+    # }
+
+    uid = request.data["uid"]
+    username = request.data["username"]
+    about_me = request.data["about_me"]
+    fluent = request.data["fluent"]
+    learning = request.data["learning"]
+    date_of_birth = request.data["date_of_birth"]
+    user_systems = request.data["systems"]
+    genre = request.data["genre"]
+    currently_playing = request.data["currently_playing"]
+    region = request.data["region"]
+    languages_column = {"fluent": fluent, "learning": learning}
+
+    Users.objects.filter(uid=uid).delete()
+
+    # structures the data how the Users table wants the payload
+    users_data = {
+        "uid": uid,
+        "username": username,
+        "date_of_birth": date_of_birth,
+        "about_me": about_me,
+        "languages": languages_column,
+        "currently_playing": currently_playing,
+        "user_systems": user_systems,
+        "user_genre": genre,
+        "user_region": region,
+    }
+
+# added the user to the Users table
+    user_serializer = UsersSerializer(data=users_data)
+    if user_serializer.is_valid():
+        user_serializer.save()
+    else:
+        errors = user_serializer.errors
+        return Response({'errors': errors}, status=400)
+
+
+# adds the languages the user is fluent in to the appropriate language tables
+    for item in fluent:
+        language = item.lower()
+        if language in model_map:
+            ModelClass, SerializerClass = model_map[language]
+            payload = {
+                "user_uid": uid,
+                "level": 4
+            }
+            serializer = SerializerClass(data=payload)
+            if serializer.is_valid():
+                serializer.save()
+            else:
+                errors = serializer.errors
+                return Response({'errors': errors}, status=400)
+
+# adds the languages the user is learning in to the appropriate language tables
+    for item in learning:
+        language = item["language"].lower()
+        level = item["level"]
+        if language in model_map:
+            ModelClass, SerializerClass = model_map[language]
+            payload = {
+                "user_uid": uid,
+                "level": level
+            }
+            serializer = SerializerClass(data=payload)
+            if serializer.is_valid():
+                serializer.save()
+            else:
+                errors = serializer.errors
+                return Response({'errors': errors}, status=400)
+            
+# adds the system the user plays on to the systems table
+    for system in user_systems:
+        payload = {
+            "user_uid": uid,
+            "system": system
+        }
+        serializer = systemsSerializer(data=payload)
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            errors = serializer.errors
+            return Response({'errors': errors}, status=400)
+            
+# adds the genre the user plays on to the genre table
+    for single_genre in genre:
+        payload = {
+            "user_uid": uid,
+            "genre": single_genre
+        }
+        serializer = genraSerializer(data=payload)
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            errors = serializer.errors
+            return Response({'errors': errors}, status=400)
+        
+    region_payload = {
+        "user_uid": uid,
+        "region": region
+    }
+    region_serializer = regionSerializer(data=region_payload)
+    if region_serializer.is_valid():
+        region_serializer.save()
+    else:
+        errors = region_serializer.errors
+        return Response({'errors': errors}, status=400)
+
+
+    return Response(
+        {
+            "uid": uid,
+            "username": username,
+            "about_me": about_me,
+            "fluent": fluent,
+            "learning": learning,
+            "date_of_birth": date_of_birth,
+            "languages_column": languages_column,
+            "systems": user_systems,
+            "genre": genre,
+            "region": region
+        }
+    )
