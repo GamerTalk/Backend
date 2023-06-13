b56c3ca07dba61894f2141931e0506ff2aa673c7
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-06-13 21:03:52 +0900

    feat: removed websocket attempt (#30)

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,6 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('api.urls')),
-    path("chat/", include("chat.urls")),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
