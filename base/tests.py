3f524582adc473414502887c097558173511e623
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-09-07 15:35:47 +0900

    feat: first test for Users model

--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Users
 
 # Create your tests here.
+
+class UserModelTest(TestCase):
+    def test_user_model_exist(self):
+        users = Users.objects.count()
+
+        self.assertEqual(users, 0)
