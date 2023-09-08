392a0d67ad671177e20235e15405281d13acc6a8
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-09-08 15:45:03 +0900

    feat: test for Users model string representation

--- base/tests.py
+++ base/tests.py
@@ -3,8 +3,29 @@ from .models import Users
 
 # Create your tests here.
 
-class UserModelTest(TestCase):
+class UsersModelTest(TestCase):
     def test_user_model_exist(self):
         users = Users.objects.count()
 
         self.assertEqual(users, 0)
+
+    def test_model_has_string_representation(self):
+        user = Users.objects.create(
+            uid="q5lS9Ac17hM1fXXmr8ZpFghvz7W2",
+            username="GodSlayerXD",
+            date_of_birth="1999-01-01",
+            about_me="I was born in a log cabin.",
+            languages={
+                "fluent": ["english", "spanish"],
+                "learning": [
+                    {"level": 1, "language": "german"},
+                    {"level": 3, "language": "japanese"},
+                ],
+            },
+            currently_playing="I am currently playing COD MW2, Fortnite, and some Ark Survival",
+            user_systems=["pc", "playstation"],
+            user_genre=["survival", "shooters"],
+            user_region="north america",
+        )
+
+        self.assertEqual(str(user), user.uid)
