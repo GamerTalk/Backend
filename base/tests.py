7019fca8ec9b01d9bf146d2e1d5b5d09c1bb99b5
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-09-14 11:47:35 +0900

    Feat: basis for models tests

--- base/tests.py
+++ base/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Users, Genre
+from .models import Users, English, Spanish, French, German, Japanese, Chinese, Korean, Systems, Genre, Messages, Region, Posts, Flashcards
 
 # Create your tests here.
 
@@ -29,3 +29,81 @@ class UsersModelTest(TestCase):
         )
 
         self.assertEqual(str(user), user.uid)
+
+class EnglishModelTest(TestCase):
+    def test_english_model_exist(self):
+        english = English.objects.count()
+
+        self.assertEqual(english, 0)
+
+class SpanishModelTest(TestCase):
+    def test_spanish_model_exist(self):
+        spanish = Spanish.objects.count()
+
+        self.assertEqual(spanish, 0)
+
+class FrenchModelTest(TestCase):
+    def test_french_model_exist(self):
+        french = French.objects.count()
+
+        self.assertEqual(french, 0)
+
+class GermanModelTest(TestCase):
+    def test_german_model_exist(self):
+        german = German.objects.count()
+
+        self.assertEqual(german, 0)
+
+class JapaneseModelTest(TestCase):
+    def test_japanese_model_exist(self):
+        japanese = Japanese.objects.count()
+
+        self.assertEqual(japanese, 0)
+
+class ChineseModelTest(TestCase):
+    def test_chinese_model_exist(self):
+        chinese = Chinese.objects.count()
+
+        self.assertEqual(chinese, 0)
+
+class KoreanModelTest(TestCase):
+    def test_korean_model_exist(self):
+        korean = Korean.objects.count()
+
+        self.assertEqual(korean, 0)
+
+class SystemsModelTest(TestCase):
+    def test_systems_model_exist(self):
+        systems = Systems.objects.count()
+
+        self.assertEqual(systems, 0)
+
+class GenreModelTest(TestCase):
+    def test_genre_model_exist(self):
+        genre = Genre.objects.count()
+
+        self.assertEqual(genre, 0)
+
+class RegionModelTest(TestCase):
+    def test_region_model_exist(self):
+        region = Region.objects.count()
+
+        self.assertEqual(region, 0)
+
+class MessagesModelTest(TestCase):
+    def test_messages_model_exist(self):
+        messages = Messages.objects.count()
+
+        self.assertEqual(messages, 0)
+
+class FlashcardsModelTest(TestCase):
+    def test_flashcards_model_exist(self):
+        flashcards = Flashcards.objects.count()
+
+        self.assertEqual(flashcards, 0)
+
+class PostsModelTest(TestCase):
+    def test_posts_model_exist(self):
+        posts = Posts.objects.count()
+
+        self.assertEqual(posts, 0)
